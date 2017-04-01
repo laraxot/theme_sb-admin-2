38512eccdfdbe83292ba890f59237c0fb7a6e92a
Raffaele <raffaele.carelle@gmail.com>
2017-04-01 15:55:48 +0200

    d

--- auth/passwords/reset.blade.php
+++ auth/passwords/reset.blade.php
@@ -1,6 +1,6 @@
-@extends('app')
+@extends ('adm_theme::layouts.plane')
 
-@section('content')
+@section('body')
 <div class="container-fluid">
 	<div class="row">
 		<div class="col-md-8 col-md-offset-2">
