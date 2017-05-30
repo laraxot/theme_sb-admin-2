9a75078ec0ae0c3639fdf5b39eeb6acfa03702d7
bubu2k <marco76tv@hotmail.com>
2017-05-30 14:04:17 +0200

    upp

--- admin/dashboard.blade.php
+++ admin/dashboard.blade.php
@@ -1,9 +1,21 @@
 @extends('adm_theme::layouts.app')
+@section('page_heading','DashBoard')
+
 @section('content')
-
-
-@foreach(Auth::User()->Areas()->get() as $area)
-	<br/><a href="{{ $area->url() }}">{{ $area->area_define_name }}</a>
-@endforeach
-
+    <div>
+        @foreach(\Auth::User()->areas()->get() as $row)
+            <div class="col-lg-2">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <small>{{$row->area_define_name}}</small>
+                    </div>
+                    <div class="panel-body" style="text-align:center;">
+                        <a href="{{ $row->a_href() }}">
+                            <img src="{{$row->controller_path}}/menu/icon.png" style="width:50px;height:50px;"/>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        @endforeach
+    </div>
 @endsection
