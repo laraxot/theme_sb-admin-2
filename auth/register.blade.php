e2985f4511a239f30bc6a8cddff5f31023cea797
bubu2k <marco76tv@hotmail.com>
2017-05-31 14:50:20 +0200

    ogni tema sia admin che pub ha il suo "login, register, lost password"

--- auth/register.blade.php
+++ auth/register.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+@extends('adm_theme::layouts.app')
 
 @section('content')
 <div class="container">
@@ -11,14 +11,14 @@
                         {{ csrf_field() }}
 
                         <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
-                            <label for="name" class="col-md-4 control-label">Name</label>
+                            <label for="username" class="col-md-4 control-label">Username</label>
 
                             <div class="col-md-6">
-                                <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}" required autofocus>
+                                <input id="username" type="text" class="form-control" name="username" value="{{ old('username') }}" required autofocus>
 
-                                @if ($errors->has('name'))
+                                @if ($errors->has('username'))
                                     <span class="help-block">
-                                        <strong>{{ $errors->first('name') }}</strong>
+                                        <strong>{{ $errors->first('username') }}</strong>
                                     </span>
                                 @endif
                             </div>
