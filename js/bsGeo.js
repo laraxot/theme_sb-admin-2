dfa87ebff84e33805a53ed72cc0c834f0101fe37
marco xot <marco.sottana@gmail.com>
2018-03-08 00:04:46 +0100

    u

--- js/bsGeo.js
+++ js/bsGeo.js
@@ -1,6 +1,6 @@
 $(function() {
 //$( document ).ready(function() {
-$.getScript(base_url+'/theme/bc/geocomplete/jquery.geocomplete.js', function() {
+$.getScript(base_url+'/theme/bc/geocomplete/jquery.geocomplete.min.js', function() {
 	$(".geocomplete").geocomplete({
      // map: ".map_canvas",
       details: "form",
