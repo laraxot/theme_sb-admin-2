3a0f7f4143ec0b60b05bada0113eb2c70248a9c0
marco76tv <marco.sottana@gmail.com>
2019-02-12 13:27:16 +0100

    themeservice

--- layouts/partials/dropdowns/user.blade.php
+++ layouts/partials/dropdowns/user.blade.php
@@ -1,26 +1,39 @@
+@php
+    //retrocompatibilita
+    if(!isset($params)){
+        $params = \Route::current()->parameters();
+    }
+    if(!isset($params['lang'])){
+        $params['lang']=\App::getLocale();
+    }   
+@endphp
 @if (\Auth::guest())
-    <li><a href="{{ route('login') }}">Login</a></li>
+    <li><a href="{{ route('login',$params) }}">Login</a></li>
     {{--<li><a href="{{ url('/register') }}">Register</a></li> --}}
 @else
 <li class="dropdown">
     <a class="dropdown-toggle" data-toggle="dropdown" href="#">
-    <i class="fa fa-user fa-fw"></i>   {{ Auth::user()->name() }} <i class="fa fa-caret-down"></i>
+        <i class="fa fa-user fa-fw"></i>   {{ Auth::user()->name() }} <i class="fa fa-caret-down"></i>
     </a>
     <ul class="dropdown-menu dropdown-user">
-        <li><a href="{{ url('/admin/profile/'.Auth::user()->auth_user_id) }}"><i class="fa fa-user fa-fw"></i> {{ trans('adm_theme::message.profile') }}</a></li>
+        <li>
+            <a href="{{ url('/admin/profile/'.Auth::user()->auth_user_id) }}">
+                <i class="fa fa-user fa-fw"></i> {{ trans('adm_theme::message.profile') }}
+            </a>
+        </li>
         {{--  <li><a href="#"><i class="fa fa-gear fa-fw"></i> Settings</a></li> --}}
         <li class="divider"></li>
         {{--  
         <li><a href="{{ url ('login') }}"><i class="fa fa-sign-out fa-fw"></i> Logout</a></li>
         --}}
         <li>
-        <a href="{{ url('/logout') }}" 
+        <a href="{{ route('logout',$params) }}" 
              onclick="event.preventDefault();
              document.getElementById('logout-form').submit();">
               <i class="fa fa-sign-out fa-fw"></i> Logout
         </a>
          <form id="logout-form" 
-                action="{{ url('/logout') }}" 
+                action="{{ route('logout',$params) }}" 
             method="POST" 
             style="display: none;">
                         {{ csrf_field() }}
